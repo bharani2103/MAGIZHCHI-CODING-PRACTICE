dcf6d0e429d42c2b3bce4bd3879ba82683e32fe8
BHARANI_21 <bharanivcet21@gmail.com>
2026-08-07 17:26:14 +0530

    hhh

---
+++ Alphabets.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Alphabets
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("ENTER THE CHARACTER : ");
+		char a=sc.next.chart(0);
+		switch (a)
+		{
+			case 'a':
+			{
+				
+				System.out.println("ALPHA  ");
+				break;
+				
+			}
+		}
+	}	
+}
